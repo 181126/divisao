f8af920382745f27dd02be4e887c92c50b939ce7
Renato Suares <renatorsc2006@gmail.com>
2023-05-30 15:29:07 -0400

    Tratamento de excessão

--- divisao/divisao/Program.cs
+++ divisao/divisao/Program.cs
@@ -3,12 +3,21 @@ class Poo
 {
 	static void Main(string[] args)
 	{
-        Console.WriteLine("informe um numero: ");
-        int a = int.Parse(Console.ReadLine());
-        Console.WriteLine("informe outro numero: ");
-        int b = int.Parse(Console.ReadLine());
-		int result = a / b;
-		Console.WriteLine(result);	
+		try 
+		{
+            Console.Write("informe um numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("informe outro numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int result = a / b;
+            Console.WriteLine(result);
+        } 
+         catch(Exception e)
+        {
+            Console.WriteLine("!!");
+        }
+        
+       	
 	} 
 }
 
